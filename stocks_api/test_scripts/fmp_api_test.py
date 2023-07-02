ba84d9ecdb95d7047fe5bb9976a51619df2da2b9
m_hofman <m.hofman@mrdm.nl>
2023-07-02 17:03:16 +0200

    tests for api

--- stocks_api/test_scripts/fmp_api_test.py
+++ stocks_api/test_scripts/fmp_api_test.py
@@ -3,9 +3,19 @@ from dotenv import load_dotenv
 import typing
 import fmpsdk
 
+
+
 # Actual API key is stored in a .env file.  Not good to store API key directly in script.
 load_dotenv()
-apikey = os.environ.get("")
+apikey = "0c613e62d120c625543dd47ad36a2d58"
+
+
+def test_load_api_keys():
+    load_dotenv()
+
+    test = os.getenv("API_KEY")
+
+    print('ok')
 
 # Company Valuation Methods
 symbol: str = "AAPL"
