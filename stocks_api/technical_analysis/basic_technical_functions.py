da42020e6df99970e1acfbac45ab1285ba65f1da
m_hofman <m.hofman@mrdm.nl>
2023-08-23 21:42:40 +0200

    added stock analysis

--- stocks_api/technical_analysis/basic_technical_functions.py
+++ stocks_api/technical_analysis/basic_technical_functions.py
@@ -1,19 +1,19 @@
 """Modules required for code"""
-# import signal
-# import sys
+
 import asyncio
-import inspect
-import aiohttp
-# import json
 import time
 import fmpsdk
 import pandas as pd
-# from IPython.display import display
 from stocks_api.start import APIFunctions, QUARTER
 
 
-class EssentialFinancialInformation:
+class Globals:
+    """Definition of global variables"""
+    STOCK_DATAFRAME = pd.DataFrame()
 
+
+class EssentialFinancialInformation:
+    """Functions for getting required financial information"""
     def __init__(self, symbol):
         self.symbol = symbol
         self.api_functions = APIFunctions()
@@ -21,67 +21,91 @@ class EssentialFinancialInformation:
         self.trades_loop = asyncio.run(self.main())
 
     async def get_financial_information(self, delay, what):
-        # await asyncio.sleep(1)
+        """Obtain financial indicators"""
         print(time.time(),'Start say_after(%s, %s)' % (delay,what))
-        await asyncio.sleep(delay)
         print(time.time(),what)
         print('now time to get financial information')
-        income_statement = fmpsdk.income_statement(apikey=self.api_key, symbol=self.symbol, period=QUARTER)
-        enterprise_values = fmpsdk.enterprise_values(apikey=self.api_key, symbol=self.symbol, period=QUARTER)
-        balance_sheet = fmpsdk.balance_sheet_statement_as_reported(apikey=self.api_key, symbol=self.symbol)
+        # income_statement = fmpsdk.income_statement(apikey=self.api_key, symbol=self.symbol, period=QUARTER)
+        # enterprise_values = fmpsdk.enterprise_values(apikey=self.api_key, symbol=self.symbol, period=QUARTER)
+        # balance_sheet = fmpsdk.balance_sheet_statement_as_reported(apikey=self.api_key, symbol=self.symbol)
+
+        indicators = ['symbol', 'date', 'period', 'priceBookValueRatio', 'priceToSalesRatio', 'priceToFreeCashFlowsRatio', 'enterpriseValueMultiple',]
+        # key_metrics = fmpsdk.key_metrics(apikey=self.api_key,symbol='LLAP', period='quarter')
+
+        ratios = fmpsdk.financial_ratios(apikey=self.api_key, symbol=self.symbol, period='quarter')
+        collection_indicators = [[(key, value) for key, value in d.items() if key in indicators] for d in ratios]
+        df_indicators = pd.DataFrame(columns=indicators)
+        # Initialize an empty dictionary
+        new_row = {}
+
+        # Loop through the list of tuples and populate the dictionary
+        for period in collection_indicators:
+            for key, value in period:
+                new_row[key] = value
+
+            df_indicators = pd.concat([df_indicators, pd.DataFrame([new_row])], ignore_index=True)
+            new_row.clear()
+
+        selected_columns = indicators[3:]
+        selected_df = df_indicators[selected_columns]
+        selected_df_pct_change = selected_df.pct_change()
+
+        # Rename columns in the resulting DataFrame
+        selected_df_pct_change.columns = [col + '_pct_change' for col in selected_columns]
+
+        # Join the calculated percentage changes back to the original DataFrame
+        result_df = pd.concat([df_indicators, selected_df_pct_change], axis=1)
+
+        Globals.STOCK_DATAFRAME = pd.concat([Globals.STOCK_DATAFRAME, result_df], ignore_index=True)
+
+        # print('ok')
 
 
-        fmpsdk.historical_chart(
 
-        )
-        print(income_statement)
-        print(enterprise_values)
-        print(balance_sheet)
 
-        return income_statement, enterprise_values
+        # df.at[1, 'Duration'] = ['30days', '35days']
+        # print(df)
+
+        # df = pd.DataFrame(
+        #     [p, p.team, p.passing_att, p.passer_rating()] for p in game.players.passing()
+        # )
+
+        # ratio of equity available to common shareholders divided by the number of outstanding shares -
+        # indication of how much shareholders are paying for the net assets of a company.
+        # price_book_value_ration = ratios['priceBookValueRatio']
+        #
+        # forward_peg_ratio = ratios[0]['priceEarningsToGrowthRatio']
+
+        # follow peg_ration with growth of stock
+
+
+        #This metric can become confusing when it turns negative and is generally not a widely-used metric.
+        # ev_ebitda_ratio = ratios['enterpriseValueMultiple']
+
+        # The P/S ratio is calculated by dividing the stock price by the underlying company's sales per share.
+        # A low ratio could imply the stock is undervalued, while a ratio that is higher-than-average could indicate
+        # that the stock is overvalued. The P/S ratio does not take into account whether the company makes any earnings
+        # or whether it will ever make earnings.
+        # price_to_sales = ratios['priceToSalesRatio']
+
+        # The higher the percentage of free cash flow embedded in a company's operating cash flow,
+        # the greater the financial strength of the company.
+
+        # price_to_free_cash_flow = ratios['priceToFreeCashFlowsRatio']
+
+        # print(income_statement)
+        # print(enterprise_values)
+        # print(balance_sheet)
+
+        return 'bla', 'bla'
+
+        # return 'bla', 'bla' income_statement, enterprise_values
 
     async def main(self):
+        """Start of async loop"""
         start_time = time.time()
         print(start_time, 'Before creating tasks.')
         task1 = asyncio.create_task(self.get_financial_information(0,'hello'))
         await task1
         end_time = time.time()
         print('Total time elapsed: %.2f seconds' % (end_time - start_time))
-
-
-class SyncEssentialFinancialInformation:
-    """Class for obtaining information for technical analysis"""
-
-    def __init__(self):
-        self.api_functions = APIFunctions()
-        self.api_key = self.api_functions.load_api_keys()
-
-    @classmethod
-    def get_technical_indicators(cls, api_key, symbol):
-        """Obtain a company's technical indications"""
-        technical_indicators = \
-            fmpsdk.technical_indicators(apikey=api_key, symbol=symbol)
-        data_frame = pd.DataFrame(technical_indicators)
-        print(data_frame)
-
-    @classmethod
-    def get_available_commodities(cls, api_key):
-        """Obtain all available commodities"""
-        return fmpsdk.available_commodities(apikey=api_key)
-
-    @classmethod
-    def calculate_ev_ebitda(cls, income_statement, enterprise_values):
-        """Evaluate the ev/ebitda value of a company"""
-        if len(income_statement) == 0 or len(enterprise_values) == 0:
-            ##TODO: add warning
-            return
-        ev_ebitda = enterprise_values[0]['enterpriseValue'] / income_statement[0]['ebitda']
-
-        print(income_statement[0]['symbol'])
-
-        ##TODO: trigger when ev_ebitda ration is <15
-
-        print(f"the ratio is {ev_ebitda}")
-
-        # if ev_ebitda < 15:
-        #     print(f"the ratio is below 15, namely {ev_ebitda}")
--- stocks_api/test_scripts/fmp_api_test.py
+++ stocks_api/test_scripts/fmp_api_test.py
@@ -1,65 +1,39 @@
 from stocks_api.start import APIFunctions, TradesList
-from stocks_api.technical_analysis.basic_technical_functions import SyncEssentialFinancialInformation, EssentialFinancialInformation
+from stocks_api.technical_analysis.basic_technical_functions import EssentialFinancialInformation
 
 
 class SimpleTestScripts:
-
-    def __init__(self, start_functions: APIFunctions, technical_functions: EssentialFinancialInformation):
+    """Class for testing iterating stocks for analysis"""
+    def __init__(self, technical_functions: EssentialFinancialInformation):
         self.api_key = self.test_load_api_key()
         self.traded_list_class = TradesList()
-
         self.traded_list = self.traded_list_class.get_traded_list(self.api_key)
         self.trade_exchange_categories = self.traded_list_class.get_trade_exchange_categories(self.traded_list)
-        print('ok')
-
         self.technical_functions = technical_functions
-        # self.stock_list = self.technical_functions.get_available_commodities(api_key=self.api_key)
         self.test_iterating_stock_list()
 
+
     def test_load_api_key(self):
+        """function to load API Key"""
         api_functions = APIFunctions()
         return api_functions.load_api_keys()
 
     def test_iterating_stock_list(self):
-        index = 0
-        # while index < 4:
-        # len(self.traded_list):
+        """function to iterate stock list"""
         for idx, trade in enumerate(self.traded_list):
             print(idx)
             if idx > 15:
                 break
-            # for trade in self.traded_list:
-            #     # print(self.traded_list[index][symbol])
-            #     # print(self.traded_list[index][symbol])
+
             if trade['type'] != 'stock':
                 print(trade['symbol'])
                 print('is not a stock: ')
                 print(trade['type'])
                 continue
-            print('now let us anaylyze a stock')
+            print('now let us analyze a stock please')
             print(trade['symbol'])
             print('exchange')
             print(trade['exchange'])
             self.technical_functions(trade['symbol'])
 
-
-
-                # income_statement = self.technical_functions.get_income_statement(api_key=self.api_key, symbol=symbol)
-                # # self.technical_functions.get_technical_indicators(api_key=self.api_key, symbol=symbol)
-                # enterprise_values = self.technical_functions.get_enterprise_values(api_key=self.api_key, symbol=symbol)
-                # self.technical_functions.calculate_ev_ebitda(income_statement, enterprise_values)
-
-        # for symbol in self.trades_list:
-        #     print(symbol)
-            # income_statement = self.technical_functions.get_income_statement(api_key=self.api_key, symbol=symbol)
-            # # self.technical_functions.get_technical_indicators(api_key=self.api_key, symbol=symbol)
-            # enterprise_values = self.technical_functions.get_enterprise_values(api_key=self.api_key, symbol=symbol)
-            # self.technical_functions.calculate_ev_ebitda(income_statement, enterprise_values)
-            # print('ok')
-    print('ok')
-
-SimpleTestScripts(APIFunctions, EssentialFinancialInformation)
-
-
-
-
+SimpleTestScripts(EssentialFinancialInformation)
