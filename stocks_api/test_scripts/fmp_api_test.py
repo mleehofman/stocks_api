464ea35abd39701aa4c716e886b8100145ca5b25
m_hofman <m.hofman@mrdm.nl>
2023-09-10 13:37:15 +0200

    ensure dataframe is empty before start

--- stocks_api/test_scripts/fmp_api_test.py
+++ stocks_api/test_scripts/fmp_api_test.py
@@ -5,12 +5,18 @@ from stocks_api.technical_analysis.basic_technical_functions import EssentialFin
 class SimpleTestScripts:
     """Class for testing iterating stocks for analysis"""
     def __init__(self, technical_functions: EssentialFinancialInformation):
+        self.empty_stocklist()
         self.api_key = self.test_load_api_key()
         self.traded_list_class = TradesList()
         self.traded_list = self.traded_list_class.get_traded_list(self.api_key)
         self.trade_exchange_categories = self.traded_list_class.get_trade_exchange_categories(self.traded_list)
         self.technical_functions = technical_functions
         self.test_iterating_stock_list()
+
+    def empty_stocklist(self):
+        Globals.STOCK_DATAFRAME.drop(Globals.STOCK_DATAFRAME.index, inplace=True)
+        empty_check = Globals.STOCK_DATAFRAME.empty
+        print(f'is the dataframe empty: {empty_check}')
 
     def test_load_api_key(self):
         """function to load API Key"""
